75cda09786a48166c630f77bf9964b9d3b6b5ca5
Alexander Meißner <AlexanderMeissner@gmx.net>
2015-05-30 12:04:47 +0200

    Updated documentation with new doxygen version

---
+++ doc/search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "abcdeghilmnoprst",
+  1: "abdehmnps",
+  2: "adgilnorst",
+  3: "chops"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "functions",
+  3: "variables"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Functions",
+  3: "Variables"
+};
+
