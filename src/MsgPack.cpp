66812ff3c14dd016a89019b028f4a1a1c867e937
Alexander Meißner <AlexanderMeissner@gmx.net>
2017-12-19 07:26:26 +0100

    Fixed BYTE_ORDER detection on windows

--- src/MsgPack.cpp
+++ src/MsgPack.cpp
@@ -21,7 +21,7 @@
 #define __builtin_bswap64 _byteswap_uint64
 #endif
 
-#if BYTE_ORDER == BIG_ENDIAN
+#if defined(BYTE_ORDER) && BYTE_ORDER == BIG_ENDIAN
 #define storeBSwap(bits) \
     memcpy(target, &source, sizeof(source));
 #define loadBSwap(bits) \
